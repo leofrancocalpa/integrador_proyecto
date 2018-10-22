527d7323048f266158cb9a9f217afa40d09f4ebb
leofrancocalpa <francocalpa@gmail.com>
2018-10-22 13:42:19 -0500

    cluster

---
+++ ProyectoAllersGroup/Model/Cluster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Cluster
+    {
+        public List<Cliente> elementos { get; set; }
+        public ItemSet agrupador;
+
+        public Cluster(ItemSet cen)
+        {
+            agrupador = cen;
+        }
+    }
+}
---
+++ ProyectoAllersGroup/Model/ClustersGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class ClustersGenerator
+    {
+        public List<Cluster> clusters { get; set; }
+
+        public ClustersGenerator()
+        {
+            clusters = new List<Cluster>();
+        }
+
+        public void GenerarClusters(Dictionary<String, Transaction> transactions, Dictionary<String, Cliente> clientes, List<ItemSet> fItemSets)
+        {
+            CrearKCentroides(fItemSets);
+        }
+
+        public void CrearKCentroides(List<ItemSet> fis)
+        {
+            foreach(ItemSet itemSet in fis)
+            {
+                Cluster cl = new Cluster(itemSet);
+                clusters.Add(cl);
+            }
+        }
+
+        public void Particionar(Dictionary<String, Transaction> transactions, Dictionary<String, Cliente> clientes)
+        {
+            foreach(Cluster cluster in clusters)
+            {
+                foreach (KeyValuePair<String, Transaction> transaccion in transactions)
+                {
+                    int valor = 0;
+                    foreach (KeyValuePair<String, Item> item in cluster.agrupador.items)
+                    {
+                        if (transaccion.Value.itemsInTransaction.items.ContainsKey(item.Key))
+                        {
+                            valor++;
+                        }
+                    }
+                    if (valor == cluster.agrupador.items.Count)
+                    {
+                        cluster.elementos.Add(clientes[transaccion.Value.codCliente]);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
